33c074786be8ed5620ac2fafb7d6c921dc7f3120
“Allen” <alnwangchi@gmail.com>
2021-10-27 00:10:29 +0800

    ScopeChain

--- B10_ScopeChain/Finished/app.js
+++ B10_ScopeChain/Finished/app.js
@@ -1,5 +1,6 @@
 function a() {
-    
+
+    // b function 在 a 被執行的時候才會被創建
     function b() {
         console.log(myVar);
     }
@@ -8,4 +9,18 @@ function a() {
 }
 
 var myVar = 1;
-a();
+// a();
+
+function cc() {
+    console.log(vava);
+}
+
+function dd() {
+    let vava = 2
+    cc()
+}
+
+let vava = 1
+dd()
+
+// 因為 cc 是定義在全域的地方，他的外層參照就是全域，所以當執行時內部找不到變數則往外至全域而非 dd ㄋ
