aee9778250453e956512579b451b97674b1cbb9a
“Allen” <alnwangchi@gmail.com>
2021-11-04 00:08:31 +0800

    this  arguments IIFE review

--- D7_ObjectFunctionsAndThis/Finished/app.js
+++ D7_ObjectFunctionsAndThis/Finished/app.js
@@ -22,7 +22,8 @@ var c = {
         console.log(self);
         
         var setname = function(newname) {
-            self.name = newname;   
+            // this.name = newname 這邊的 this 是指向全域，他就是這樣
+            self.name = newname; // 所以在上一層先把 this 存起來   
         }
         setname('Updated again! The c object');
         console.log(self);
